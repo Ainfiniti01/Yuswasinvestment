42b7990dc31dac1e4a6bfad27dfb731a9c30baee
Ainfiniti <abdulazeezadam09@gmail.com>
2026-09-14 22:52:34 +0100

    Add favicon link to root component

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -434,6 +434,7 @@ export function Layout({ children }: { children: ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/png" href="/images/Yuswas%20logo.png" />
         <Meta />
         <Links />
         {import.meta.env.DEV ? (
